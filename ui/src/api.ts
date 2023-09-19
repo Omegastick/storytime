cb06efe548a3667cded33c3815e62a30d360b711
omegastick <isaac@poulton.dev>
2023-09-19 01:12:13 +0100

    Allow setting custom API URL

--- ui/src/api.ts
+++ ui/src/api.ts
@@ -1,15 +1,24 @@
-import axios from 'axios';
+import axios from "axios";
 
 export const postToTTS = async (text: string): Promise<ArrayBuffer> => {
+  const rawApiUrl = localStorage.getItem("apiUrl");
+  if (!rawApiUrl) {
+    throw new Error("No API URL set");
+  }
+  const apiUrl = JSON.parse(rawApiUrl);
   try {
-    const response = await axios.post('http://localhost:8000/tts', {
-      text,
-    }, {
-      responseType: 'arraybuffer',
-    });
+    const response = await axios.post(
+      `http://${apiUrl}/tts`,
+      {
+        text: text,
+      },
+      {
+        responseType: "arraybuffer",
+      }
+    );
     return response.data;
   } catch (error) {
-    console.error('Error posting text to TTS:', error);
+    console.error("Error posting text to TTS:", error);
     throw error;
   }
 };
--- ui/src/components/TTSPage.tsx
+++ ui/src/components/TTSPage.tsx
@@ -1,6 +1,7 @@
-import { Box, IconButton, Textarea } from "@chakra-ui/react";
+import { Box, IconButton, Input, Textarea } from "@chakra-ui/react";
 import React, { useCallback } from "react";
 import { BsFillPauseFill, BsFillPlayFill } from "react-icons/bs";
+import useLocalStorage from "use-local-storage";
 import { useAudio } from "../hooks/useAudio";
 import { useAudioBufferQueue } from "../hooks/useAudioBufferQueue";
 import { useTextChunker } from "../hooks/useTextChunker";
@@ -10,6 +11,7 @@ type TTSPageProps = {};
 const TTSPage: React.FC<TTSPageProps> = () => {
   const { setText, chunks } = useTextChunker();
   const { nextAudioBuffer, clearQueue } = useAudioBufferQueue(chunks);
+  const [apiUrl, setApiUrl] = useLocalStorage("apiUrl", "localhost:8000");
 
   const onAudioEnded = useCallback(async () => {
     return nextAudioBuffer();
@@ -45,6 +47,7 @@ const TTSPage: React.FC<TTSPageProps> = () => {
 
   return (
     <Box display="flex" flexDirection="column" height="100%">
+      <Input value={apiUrl} onChange={(e) => setApiUrl(e.target.value)} />
       <Textarea
         flexGrow={1}
         placeholder="Write something..."
